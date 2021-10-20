99de5c70f1a23cbbd553fb432e36b58f4f22d6a8
Alex Marshall <alex.k.marshall83@gmail.com>
2021-10-20 16:10:45 +0100

    style metric card

--- src/components/metric-card.css.ts
+++ src/components/metric-card.css.ts
@@ -1,10 +1,62 @@
 import { box } from "src/styles/layout.css"
+import { recipe } from "@vanilla-extract/recipes"
 import { style } from "@vanilla-extract/css"
+import { themeVars } from "src/styles/global.css"
 
 export const card = style([
   box,
   {
     display: "grid",
     gridTemplateColumns: "repeat(2, 1fr)",
+    gap: "1.5rem",
+    borderRadius: "5px",
   },
 ])
+
+export const justifyEnd = style({
+  justifySelf: "end",
+})
+
+export const alignEnd = style({
+  alignSelf: "end",
+})
+
+export const metric = style({
+  fontSize: themeVars.typography.fontSize.small,
+  fontWeight: 700,
+})
+
+export const value = style([
+  alignEnd,
+  {
+    fontSize: themeVars.typography.fontSize.large,
+    fontWeight: 700,
+    lineHeight: "1",
+  },
+])
+
+export const change = recipe({
+  base: [
+    alignEnd,
+    justifyEnd,
+    {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      gap: "0.25rem",
+      fontSize: themeVars.typography.fontSize.extraSmall,
+      fontWeight: 700,
+    },
+  ],
+
+  variants: {
+    direction: {
+      up: {
+        color: themeVars.color.green,
+      },
+      down: {
+        color: themeVars.color.red,
+      },
+    },
+  },
+})
--- src/components/metric-card.tsx
+++ src/components/metric-card.tsx
@@ -1,6 +1,8 @@
+import * as styles from "./metric-card.css"
+
 import { Brand } from "src/types"
+import { Chevron } from "./chevron"
 import { Logo } from "src/components/logo"
-import { card } from "./metric-card.css"
 import { formatNumber } from "src/number-format"
 import { screenReaderOnly } from "src/styles/accessibility.css"
 import { useId } from "@react-aria/utils"
@@ -21,15 +23,16 @@ export function MetricCard({
   const changeDirection = changePercent >= 0 ? "up" : "down"
 
   return (
-    <article aria-labelledby={headingId} className={card}>
-      <h3 id={headingId}>
+    <article aria-labelledby={headingId} className={styles.card}>
+      <h3 id={headingId} className={styles.metric}>
         <span>{metric}</span>
         <span className={screenReaderOnly}> {brand}</span>
       </h3>
-      <Logo brand={brand} aria-hidden />
-      <p>{formatNumber(value)}</p>
-      <p>
-        <span>{changeDirection}</span>
+      <Logo brand={brand} aria-hidden className={styles.justifyEnd} />
+      <p className={styles.value}>{formatNumber(value)}</p>
+      <p className={styles.change({ direction: changeDirection })}>
+        <span className={screenReaderOnly}>{changeDirection}</span>
+        <Chevron direction={changeDirection} aria-hidden />
         <span>{Math.abs(changePercent)}%</span>
       </p>
     </article>
